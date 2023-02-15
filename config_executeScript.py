905e86127d2e7eb431712662a12a4c0ca88a4efb
Mart Bluiminck VS <m.bluiminck@dgs-ps.nl>
2023-02-15 09:48:34 +0100

    Check functionality both devices

--- config_executeScript.py
+++ config_executeScript.py
@@ -1,5 +1,5 @@
 ## INPUTS
-file_nr = 12345
+file_nr = 0
 Available_files =   [   "AquireImageFromFRAMOS.py"   ,       #   0
                         "test.py"                    ,       #   1
                     ]
