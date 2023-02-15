ccb1e9a91c9c3100c2976a62378df3e4d6c4a24e
DGS_Jetson_Nano <rddgs@dgs-ps.nl>
2023-02-15 09:46:43 +0100

    Update from PI

--- config_executeScript.py
+++ config_executeScript.py
@@ -1,5 +1,5 @@
 ## INPUTS
-file_nr = 123
+file_nr = 12345
 Available_files =   [   "AquireImageFromFRAMOS.py"   ,       #   0
                         "test.py"                    ,       #   1
                     ]
