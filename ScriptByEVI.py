78ba19820a58404a927be576a8598f191981f8f6
Mart Bluiminck VS <m.bluiminck@dgs-ps.nl>
2023-02-16 08:40:28 +0100

    test Tensorflow

--- ScriptByEVI.py
+++ ScriptByEVI.py
@@ -3,6 +3,8 @@ import numpy as np
 import cv2
 import subprocess, time 
 import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Flatten, Dropout
 
 ## Defined library locations of images
 loc_def_raspberry  = "dgslr@192.168.23.251:/home/dgslr/ProgramFiles/SCP_images/"  
@@ -17,3 +19,27 @@ print("is_cuda_gpu_min_3: ", is_cuda_gpu_min_3)
 
 # Doe hiermee wat je wilt!
 
+# Avoid OOM errors by setting GPU Memory Consumption Growth
+gpus = tf.config.experimental.list_physical_devices('GPU')
+for gpu in gpus: 
+    tf.config.experimental.set_memory_growth(gpu, True)
+
+model = Sequential()
+model.add(Conv2D(16, (3,3), 1, activation='relu', input_shape=(256,256,3)))
+model.add(MaxPooling2D())
+
+model.add(Conv2D(32, (3,3), 1, activation='relu'))
+model.add(MaxPooling2D())
+
+model.add(Conv2D(16, (3,3), 1, activation='relu'))
+model.add(MaxPooling2D())
+
+model.add(Flatten())
+model.add(Dense(256, activation='relu'))
+model.add(Dense(3, activation='relu'))
+
+#tf.optimizers.
+model.compile('adam', loss=tf.losses.BinaryCrossentropy(), metrics=['accuracy'])
+model.summary()
+
+model.summary()
