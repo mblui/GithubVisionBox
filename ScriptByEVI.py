8d7f7d92e5e72979119f669852fa40aa8fd1c619
Mart Bluiminck VS <m.bluiminck@dgs-ps.nl>
2023-02-16 09:09:48 +0100

    Tensorflow

--- ScriptByEVI.py
+++ ScriptByEVI.py
@@ -1,10 +1,10 @@
 import pyrealsense2 as rs
 import numpy as np
-import cv2
-import subprocess, time 
+import os
+import glob
+import xml.etree.ElementTree as ET
+import pandas as pd
 import tensorflow as tf
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Flatten, Dropout
 
 ## Defined library locations of images
 loc_def_raspberry  = "dgslr@192.168.23.251:/home/dgslr/ProgramFiles/SCP_images/"  
@@ -15,14 +15,7 @@ is_cuda_gpu_available = tf.test.is_gpu_available(cuda_only=True)
 is_cuda_gpu_min_3 = tf.test.is_gpu_available(True, (3,0))
 
 print("is_cuda_gpu_available: ", is_cuda_gpu_available)
-print("is_cuda_gpu_min_3: ", is_cuda_gpu_min_3)
-
 # Doe hiermee wat je wilt! Succes! 
 
-## Import Libraries
-import os
-import glob
-import xml.etree.ElementTree as ET
-import pandas as pd
-import tensorflow as tf
+
 print(tf.__version__)
