59500cb442c3116dfc05a1f06ca2d372a346464b
Mart Bluiminck VS <m.bluiminck@dgs-ps.nl>
2023-02-17 15:16:03 +0100

    1212121

--- custom_training/main_test_CT.py
+++ custom_training/main_test_CT.py
@@ -30,11 +30,13 @@ if 0:
     print("Total matches =", i)
 
 ###############################################################
+
+import numpy as np
+import tensorflow as tf
 gpus = tf.config.experimental.list_physical_devices('GPU') 
 for gpu in gpus:
 	tf.config.experimental.set_memory_growth(gpu, True)
-import numpy as np
-
+        
 import matplotlib.pyplot as plt
 # import tensorflow as tf
 # from keras.models import Sequential
