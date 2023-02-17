e9ea86525c4bb5549066e104c65e2ed3bdc9ef5d
Mart Bluiminck VS <m.bluiminck@dgs-ps.nl>
2023-02-17 14:41:10 +0100

    1212121

--- custom_training/main_test_CT.py
+++ custom_training/main_test_CT.py
@@ -14,7 +14,7 @@ for f in os.listdir(path_GIT+allfolders[1]):
     for ff in os.listdir(path_GIT+allfolders[0]):
         #print("image", ff)
         if f[:-4] == ff[:-4]:
-            destimation = path_GIT + allfolders[2] + ff
+            destimation = path_GIT + allfolders[2]
             origin = path_GIT +  allfolders[0] + ff
             print("destimation", destimation)
             print("origin", origin)
@@ -22,18 +22,6 @@ for f in os.listdir(path_GIT+allfolders[1]):
             cv2.waitKey(0)
             #print("destimation", destimation)
             #print("origin", origin)
-            shutil.copyfile(origin, destimation)
-            i = i + 1
-print("Total matches =", i)
-
-i = 0
-for f in os.listdir(path_GIT+allfolders[3]):
-    for ff in os.listdir(path_GIT+allfolders[0]):
-        if f[:-4] == ff[:-4]:
-            destimation = path_GIT + allfolders[4] + ff
-            origin = path_GIT +  allfolders[0]+ff
-            cv2.imshow('hoi', cv2.imread(origin,2))
-            cv2.waitKey(150)
             shutil.copyfile(origin, destimation)
             i = i + 1
 print("Total matches =", i)
