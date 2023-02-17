4ca6c1918db2fc0d7666ebcfabec10e07dfc555a
Mart Bluiminck VS <m.bluiminck@dgs-ps.nl>
2023-02-17 11:06:15 +0100

    12121

--- process_image.py
+++ process_image.py
@@ -75,7 +75,8 @@ def get_output_image(path):
     areas = [cv2.contourArea(c) for c in contours]
     max_index = np.argmax(areas)
     cnt=contours[max_index]
-    print("hierarchy", hierarchy.size)
+    hier = hierarchy[:,max_index]
+    print("hierarchy", hier)
     #hierarchy = hierarchy[max_index]
     contours = cnt
     print("Length fo contours2", len(contours))
