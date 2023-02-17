2aaa5c42539070791996d08f875a840c7193b540
Mart Bluiminck VS <m.bluiminck@dgs-ps.nl>
2023-02-17 15:05:15 +0100

    1212

--- custom_training/main_test_CT.py
+++ custom_training/main_test_CT.py
@@ -34,13 +34,15 @@ if 0:
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
-from keras.models import Sequential
-from keras.layers.core import Dense, Dropout, Activation, Flatten
-from keras.layers import Conv2D, MaxPool2D
-from keras import optimizers
-from keras.datasets import mnist
-from keras.utils import to_categorical
-import keras
+# import tensorflow as tf
+# from keras.models import Sequential
+# from keras.layers.core import Dense, Dropout, Activation, Flatten
+# from keras.layers import Conv2D, MaxPool2D
+# from keras import optimizers
+# from keras.datasets import mnist
+# from keras.utils import to_categorical
+import tensorflow.keras as keras
+from tensorflow.keras.models import load_model
 
 import pandas as pd
 import os, sys 
