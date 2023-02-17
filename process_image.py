9b4fc424df5b153274cd8de78f2410192cfc9385
Mart Bluiminck VS <m.bluiminck@dgs-ps.nl>
2023-02-17 09:53:26 +0100

    comment tensorflow

--- process_image.py
+++ process_image.py
@@ -1,12 +1,12 @@
 import cv2
 import numpy as np        
 import matplotlib.pyplot as plt
-#from scipy import ndimage
+from scipy import ndimage
 import math
 #import tensorflow.keras as keras
 #from tensorflow.keras.models import load_model
 
-import tensorflow as tf
+#import tensorflow as tf
 #gpus = tf.config.experimental.list_physical_devices('GPU') 
 #for gpu in gpus:
 	#tf.config.experimental.set_memory_growth(gpu, True)
