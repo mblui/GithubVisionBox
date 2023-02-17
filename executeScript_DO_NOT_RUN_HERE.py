6ed89b6ff7c7618a66c20b9d140dcc5beb6df730
Mart Bluiminck VS <m.bluiminck@dgs-ps.nl>
2023-02-17 09:23:16 +0100

    1212

--- executeScript_DO_NOT_RUN_HERE.py
+++ executeScript_DO_NOT_RUN_HERE.py
@@ -13,22 +13,25 @@ src_path = r"/home/jetson/Documents/GithHub/GithubVisionBox/"
 ## Add github path to system path
 sys.path.append(src_path)
 from config_executeScript import *
-
-## Load Configuration file
-file_path = Available_files[file_nr]
-
-## Last modified state
-ti_m = os.path.getmtime(src_path+file_path)
-m_ti = time.ctime(ti_m)
+file_path = []
+ti_m =[]
+m_ti =[]
 
 print("##############################")
 print("## Start Python Script")
-print("##", file_path, "was last modified at" , m_ti)
-print("## Loading: ", file_path, "--> 0%")  
-for file in file_path:
-    shutil.copy(src_path+file, dst_path)
-print("## Loading: ", file_path, "--> 100%")  
-print("## Executing: ", file_path)
+## Load Configuration file
+for fileIndex in range(len(file_nr)):
+    file_path.append(Available_files[file_nr[fileIndex]])
+    ## Last modified state
+    ti_m.append(os.path.getmtime(src_path+file_path[fileIndex]))
+    m_ti.append(time.ctime(ti_m[fileIndex]))
+    print("##", file_path[fileIndex], "was last modified at" , m_ti[fileIndex])
+    print("## Loading: ", file_path[fileIndex], "--> 0%")  
+    #shutil.copy(src_path+file_path[fileIndex], dst_path)
+    print("## Loading: ", file_path[fileIndex], "--> 100%")  
+
+print("Filepath", file_path)
+print("## Executing: ", file_path[0])
 print("##############################")
 
 
