7ac275c060d3c84b8157864e430f04a4837766a6
Mart Bluiminck VS <m.bluiminck@dgs-ps.nl>
2023-02-17 15:15:32 +0100

    121212

--- custom_training/main_test_CT.py
+++ custom_training/main_test_CT.py
@@ -30,6 +30,9 @@ if 0:
     print("Total matches =", i)
 
 ###############################################################
+gpus = tf.config.experimental.list_physical_devices('GPU') 
+for gpu in gpus:
+	tf.config.experimental.set_memory_growth(gpu, True)
 import numpy as np
 
 import matplotlib.pyplot as plt
