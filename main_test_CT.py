51ed4f8b72887f0eac36111b17168690ced6fa58
Mart Bluiminck | DGS Processing Solutions <m.bluiminck@dgs-ps.nl>
2023-02-20 13:59:23 +0100

    1212121

--- main_test_CT.py
+++ main_test_CT.py
@@ -101,11 +101,14 @@ else:
     dir_preprocessed = customFolder
     df_anno.to_csv(os.path.join(dir_preprocessed,"df_anno.csv"),index=False)
 #############################################
-plt.hist(df_anno["Nobj"].values,bins=100)
-plt.title("max N of objects per image={}".format(maxNobj))
-plt.show()
+#plt.hist(df_anno["Nobj"].values,bins=100)
+#plt.title("max N of objects per image={}".format(maxNobj))
+#plt.show()
+
 
 ####################################################
+## Show how classes are distributed
+    
 from collections import Counter
 class_obj = []
 for ibbx in range(maxNobj):
@@ -126,3 +129,46 @@ plt.barh(xs,class_count)
 plt.yticks(xs,class_nm)
 plt.title("The number of objects per class: {} objects in total".format(len(count)))
 plt.show()
+####################################################
+## SHOW arbitrary image with label
+import imageio
+def plt_rectangle(plt,label,x1,y1,x2,y2):
+    '''
+    == Input ==
+    
+    plt   : matplotlib.pyplot object
+    label : string containing the object class name
+    x1    : top left corner x coordinate
+    y1    : top left corner y coordinate
+    x2    : bottom right corner x coordinate
+    y2    : bottom right corner y coordinate
+    '''
+    linewidth = 3
+    color = "yellow"
+    plt.text(x1,y1,label,fontsize=20,backgroundcolor="magenta")
+    plt.plot([x1,x1],[y1,y2], linewidth=linewidth,color=color)
+    plt.plot([x2,x2],[y1,y2], linewidth=linewidth,color=color)
+    plt.plot([x1,x2],[y1,y1], linewidth=linewidth,color=color)
+    plt.plot([x1,x2],[y2,y2], linewidth=linewidth,color=color)
+    
+# randomly select 20 frames    
+size = 20    
+ind_random = np.random.randint(0,df_anno.shape[0],size=size)
+for irow in ind_random:
+    row  = df_anno.iloc[irow,:]
+    path = os.path.join(img_dir, row["fileID"] + ".jpg")
+    # read in image
+    img  = imageio.imread(path)
+
+    plt.figure(figsize=(12,12))
+    plt.imshow(img) # plot image
+    plt.title("Nobj={}, height={}, width={}".format(row["Nobj"],row["height"],row["width"]))
+    # for each object in the image, plot the bounding box
+    for iplot in range(row["Nobj"]):
+        plt_rectangle(plt,
+                      label = row["bbx_{}_name".format(iplot)],
+                      x1=row["bbx_{}_xmin".format(iplot)],
+                      y1=row["bbx_{}_ymin".format(iplot)],
+                      x2=row["bbx_{}_xmax".format(iplot)],
+                      y2=row["bbx_{}_ymax".format(iplot)])
+    plt.show() ## show the plot
