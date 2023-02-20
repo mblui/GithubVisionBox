a4551abb30c36598d79fc8777dac74baa06a6016
Mart Bluiminck | DGS Processing Solutions <m.bluiminck@dgs-ps.nl>
2023-02-20 13:32:48 +0100

    12121

--- main_test_CT.py
+++ main_test_CT.py
@@ -2,57 +2,55 @@ import os
 import shutil
 
 ## Disable GPU
-os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+#os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 
 
-## Import once labels and files
-if 0:
-    path_GIT = "/home/jetson/Documents/GithHub/GithubVisionBox/custom_training/"
-    allfolders = ["JPEGImages/", "data_in/test_labels", "data_in/", "data_in/train_labels", "data_in/"]
-    # hoi
-    valid_images = [".jpg",".gif",".png",".tga"]
-    i = 0
-    os.chdir(path_GIT)
-    print("Current directory", os.getcwd())
-    for f in os.listdir(path_GIT+allfolders[3]):
-        #print("XML FILE", f)
-        for ff in os.listdir(path_GIT+allfolders[0]):
-            #print("image", ff)
-            if f[:-4] == ff[:-4]:
-                destimation = path_GIT + allfolders[2] + ff
-                origin = path_GIT +  allfolders[0] + ff
-                print("destimation", destimation)
-                print("origin", origin)
-                cv2.imshow('hoi', cv2.imread(origin,2))
-                cv2.waitKey(150)
-                #print("destimation", destimation)
-                #print("origin", origin)
-                shutil.copyfile(origin, destimation)
-                i = i + 1
-    print("Total matches =", i)
+# ## Import once labels and files
+# if 0:
+#     path_GIT = "/home/jetson/Documents/GithHub/GithubVisionBox/custom_training/"
+#     allfolders = ["JPEGImages/", "data_in/test_labels", "data_in/", "data_in/train_labels", "data_in/"]
+#     # hoi
+#     valid_images = [".jpg",".gif",".png",".tga"]
+#     i = 0
+#     os.chdir(path_GIT)
+#     print("Current directory", os.getcwd())
+#     for f in os.listdir(path_GIT+allfolders[3]):
+#         #print("XML FILE", f)
+#         for ff in os.listdir(path_GIT+allfolders[0]):
+#             #print("image", ff)
+#             if f[:-4] == ff[:-4]:
+#                 destimation = path_GIT + allfolders[2] + ff
+#                 origin = path_GIT +  allfolders[0] + ff
+#                 print("destimation", destimation)
+#                 print("origin", origin)
+#                 cv2.imshow('hoi', cv2.imread(origin,2))
+#                 cv2.waitKey(150)
+#                 #print("destimation", destimation)
+#                 #print("origin", origin)
+#                 shutil.copyfile(origin, destimation)
+#                 i = i + 1
+#     print("Total matches =", i)
 
 ###############################################################
 
 import numpy as np
-import tensorflow as tf
-gpus = tf.config.experimental.list_physical_devices('GPU') 
-for gpu in gpus:
-	tf.config.experimental.set_memory_growth(gpu, True)
+#import tensorflow as tf
+#gpus = tf.config.experimental.list_physical_devices('GPU') 
+#for gpu in gpus:
+#	tf.config.experimental.set_memory_growth(gpu, True)
         
-import matplotlib.pyplot as plt
-import tensorflow.keras as keras
-from tensorflow.keras.models import load_model
-import tensorflow as tf
-from tensorflow.keras.applications import VGG16
-import os 
-import numpy as np
+#import matplotlib.pyplot as plt
+#import tensorflow.keras as keras
+#from tensorflow.keras.models import load_model
+#from tensorflow.keras.applications import VGG16
+#import os 
 import xml.etree.ElementTree as ET
 from collections import OrderedDict
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import pandas as pd 
 
 
-customFolder = "GithubVisionBox/custom_training/result/"
+customFolder = "custom_training/result/"
 dir_anno = customFolder + "Annotations"
 img_dir  = customFolder + "JPEGImages"
 
@@ -76,6 +74,7 @@ def extract_single_xml_file(tree):
     return(row)
 
 df_anno = []
+print("dir_anno", dir_anno)
 for fnm in os.listdir(dir_anno):  
     if not fnm.startswith('.'): ## do not include hidden folders/files
         tree = ET.parse(os.path.join(dir_anno,fnm))
