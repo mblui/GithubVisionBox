16a0fdb4239709244e2a283ea7f8505477eedd97
Mart Bluiminck VS <m.bluiminck@dgs-ps.nl>
2023-03-08 15:32:49 +0100

    1212121

--- ObjectDetectionYolo-master/part_3_object_detection_with_yolo_using_voc_2012_data_model.py
+++ ObjectDetectionYolo-master/part_3_object_detection_with_yolo_using_voc_2012_data_model.py
@@ -80,193 +80,195 @@ from tensorflow.keras.optimizers import SGD, Adam, RMSprop
 from tensorflow.keras.layers import concatenate
 import tensorflow.keras.backend as K
 import tensorflow as tf
-print(" ###Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+
+print(" ### Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
 gpus = tf.config.list_physical_devices('GPU')
 for gpu in gpus:
     tf.config.experimental.set_memory_growth(gpu, True)
 
-# the function to implement the orgnization layer (thanks to github.com/allanzelener/YAD2K)
-def space_to_depth_x2(x):
-    return tf.space_to_depth(x, block_size=2)
+# # the function to implement the orgnization layer (thanks to github.com/allanzelener/YAD2K)
+# def space_to_depth_x2(x):
+#     return tf.space_to_depth(x, block_size=2)
 
-def ConvBatchLReLu(x,filters,kernel_size,index,trainable):
-    # when strides = None, strides = pool_size.
-    x = Conv2D(filters, kernel_size, strides=(1,1), 
-               padding='same', name='conv_{}'.format(index), 
-               use_bias=False, trainable=trainable)(x)
-    x = BatchNormalization(name='norm_{}'.format(index), trainable=trainable)(x)
-    x = LeakyReLU(alpha=0.1)(x)
-    return(x)
-def ConvBatchLReLu_loop(x,index,convstack,trainable):
-    for para in convstack:
-        x = ConvBatchLReLu(x,para["filters"],para["kernel_size"],index,trainable)
-        index += 1
-    return(x)
-def define_YOLOv2(IMAGE_H,IMAGE_W,GRID_H,GRID_W,TRUE_BOX_BUFFER,BOX,CLASS, trainable=False):
-    convstack3to5  = [{"filters":128, "kernel_size":(3,3)},  # 3
-                      {"filters":64,  "kernel_size":(1,1)},  # 4
-                      {"filters":128, "kernel_size":(3,3)}]  # 5
+# def ConvBatchLReLu(x,filters,kernel_size,index,trainable):
+#     # when strides = None, strides = pool_size.
+#     x = Conv2D(filters, kernel_size, strides=(1,1), 
+#                padding='same', name='conv_{}'.format(index), 
+#                use_bias=False, trainable=trainable)(x)
+#     x = BatchNormalization(name='norm_{}'.format(index), trainable=trainable)(x)
+#     x = LeakyReLU(alpha=0.1)(x)
+#     return(x)
+# def ConvBatchLReLu_loop(x,index,convstack,trainable):
+#     for para in convstack:
+#         x = ConvBatchLReLu(x,para["filters"],para["kernel_size"],index,trainable)
+#         index += 1
+#     return(x)
+# def define_YOLOv2(IMAGE_H,IMAGE_W,GRID_H,GRID_W,TRUE_BOX_BUFFER,BOX,CLASS, trainable=False):
+#     convstack3to5  = [{"filters":128, "kernel_size":(3,3)},  # 3
+#                       {"filters":64,  "kernel_size":(1,1)},  # 4
+#                       {"filters":128, "kernel_size":(3,3)}]  # 5
                     
-    convstack6to8  = [{"filters":256, "kernel_size":(3,3)},  # 6
-                      {"filters":128, "kernel_size":(1,1)},  # 7
-                      {"filters":256, "kernel_size":(3,3)}]  # 8
+#     convstack6to8  = [{"filters":256, "kernel_size":(3,3)},  # 6
+#                       {"filters":128, "kernel_size":(1,1)},  # 7
+#                       {"filters":256, "kernel_size":(3,3)}]  # 8
     
-    convstack9to13 = [{"filters":512, "kernel_size":(3,3)},  # 9
-                      {"filters":256, "kernel_size":(1,1)},  # 10
-                      {"filters":512, "kernel_size":(3,3)},  # 11
-                      {"filters":256, "kernel_size":(1,1)},  # 12
-                      {"filters":512, "kernel_size":(3,3)}]  # 13
+#     convstack9to13 = [{"filters":512, "kernel_size":(3,3)},  # 9
+#                       {"filters":256, "kernel_size":(1,1)},  # 10
+#                       {"filters":512, "kernel_size":(3,3)},  # 11
+#                       {"filters":256, "kernel_size":(1,1)},  # 12
+#                       {"filters":512, "kernel_size":(3,3)}]  # 13
         
-    convstack14to20 = [{"filters":1024, "kernel_size":(3,3)}, # 14 
-                       {"filters":512,  "kernel_size":(1,1)}, # 15
-                       {"filters":1024, "kernel_size":(3,3)}, # 16
-                       {"filters":512,  "kernel_size":(1,1)}, # 17
-                       {"filters":1024, "kernel_size":(3,3)}, # 18
-                       {"filters":1024, "kernel_size":(3,3)}, # 19
-                       {"filters":1024, "kernel_size":(3,3)}] # 20
+#     convstack14to20 = [{"filters":1024, "kernel_size":(3,3)}, # 14 
+#                        {"filters":512,  "kernel_size":(1,1)}, # 15
+#                        {"filters":1024, "kernel_size":(3,3)}, # 16
+#                        {"filters":512,  "kernel_size":(1,1)}, # 17
+#                        {"filters":1024, "kernel_size":(3,3)}, # 18
+#                        {"filters":1024, "kernel_size":(3,3)}, # 19
+#                        {"filters":1024, "kernel_size":(3,3)}] # 20
     
-    input_image = Input(shape=(IMAGE_H, IMAGE_W, 3),name="input_image")
-    true_boxes  = Input(shape=(1, 1, 1, TRUE_BOX_BUFFER , 4),name="input_hack")    
-    # Layer 1
-    x = ConvBatchLReLu(input_image,filters=32,kernel_size=(3,3),index=1,trainable=trainable)
+#     input_image = Input(shape=(IMAGE_H, IMAGE_W, 3),name="input_image")
+#     true_boxes  = Input(shape=(1, 1, 1, TRUE_BOX_BUFFER , 4),name="input_hack")    
+#     # Layer 1
+#     x = ConvBatchLReLu(input_image,filters=32,kernel_size=(3,3),index=1,trainable=trainable)
     
-    x = MaxPooling2D(pool_size=(2, 2),name="maxpool1_416to208")(x)
-    # Layer 2
-    x = ConvBatchLReLu(x,filters=64,kernel_size=(3,3),index=2,trainable=trainable)
-    x = MaxPooling2D(pool_size=(2, 2),name="maxpool1_208to104")(x)
+#     x = MaxPooling2D(pool_size=(2, 2),name="maxpool1_416to208")(x)
+#     # Layer 2
+#     x = ConvBatchLReLu(x,filters=64,kernel_size=(3,3),index=2,trainable=trainable)
+#     x = MaxPooling2D(pool_size=(2, 2),name="maxpool1_208to104")(x)
     
-    # Layer 3 - 5
-    x = ConvBatchLReLu_loop(x,3,convstack3to5,trainable)
-    x = MaxPooling2D(pool_size=(2, 2),name="maxpool1_104to52")(x)
+#     # Layer 3 - 5
+#     x = ConvBatchLReLu_loop(x,3,convstack3to5,trainable)
+#     x = MaxPooling2D(pool_size=(2, 2),name="maxpool1_104to52")(x)
     
-    # Layer 6 - 8 
-    x = ConvBatchLReLu_loop(x,6,convstack6to8,trainable)
-    x = MaxPooling2D(pool_size=(2, 2),name="maxpool1_52to26")(x) 
+#     # Layer 6 - 8 
+#     x = ConvBatchLReLu_loop(x,6,convstack6to8,trainable)
+#     x = MaxPooling2D(pool_size=(2, 2),name="maxpool1_52to26")(x) 
 
-    # Layer 9 - 13
-    x = ConvBatchLReLu_loop(x,9,convstack9to13,trainable)
+#     # Layer 9 - 13
+#     x = ConvBatchLReLu_loop(x,9,convstack9to13,trainable)
         
-    skip_connection = x
-    x = MaxPooling2D(pool_size=(2, 2),name="maxpool1_26to13")(x)
+#     skip_connection = x
+#     x = MaxPooling2D(pool_size=(2, 2),name="maxpool1_26to13")(x)
     
-    # Layer 14 - 20
-    x = ConvBatchLReLu_loop(x,14,convstack14to20,trainable)
+#     # Layer 14 - 20
+#     x = ConvBatchLReLu_loop(x,14,convstack14to20,trainable)
 
-    # Layer 21
-    skip_connection = ConvBatchLReLu(skip_connection,filters=64,
-                                     kernel_size=(1,1),index=21,trainable=trainable)
-    skip_connection = Lambda(space_to_depth_x2)(skip_connection)
+#     # Layer 21
+#     skip_connection = ConvBatchLReLu(skip_connection,filters=64,
+#                                      kernel_size=(1,1),index=21,trainable=trainable)
+#     skip_connection = Lambda(space_to_depth_x2)(skip_connection)
 
-    x = concatenate([skip_connection, x])
+#     x = concatenate([skip_connection, x])
 
-    # Layer 22
-    x = ConvBatchLReLu(x,filters=1024,kernel_size=(3,3),index=22,trainable=trainable)
+#     # Layer 22
+#     x = ConvBatchLReLu(x,filters=1024,kernel_size=(3,3),index=22,trainable=trainable)
 
-    # Layer 23
-    x = Conv2D(BOX * (4 + 1 + CLASS), (1,1), strides=(1,1), padding='same', name='conv_23')(x)
-    output = Reshape((GRID_H, GRID_W, BOX, 4 + 1 + CLASS),name="final_output")(x)
+#     # Layer 23
+#     x = Conv2D(BOX * (4 + 1 + CLASS), (1,1), strides=(1,1), padding='same', name='conv_23')(x)
+#     output = Reshape((GRID_H, GRID_W, BOX, 4 + 1 + CLASS),name="final_output")(x)
 
-    # small hack to allow true_boxes to be registered when Keras build the model 
-    # for more information: https://github.com/fchollet/keras/issues/2790
-    output = Lambda(lambda args: args[0],name="hack_layer")([output, true_boxes])
+#     # small hack to allow true_boxes to be registered when Keras build the model 
+#     # for more information: https://github.com/fchollet/keras/issues/2790
+#     output = Lambda(lambda args: args[0],name="hack_layer")([output, true_boxes])
 
-    model = Model([input_image, true_boxes], output)
-    return(model, true_boxes)
+#     model = Model([input_image, true_boxes], output)
+#     return(model, true_boxes)
 
-IMAGE_H, IMAGE_W  = 416, 416
-GRID_H,  GRID_W   = 13 , 13
-TRUE_BOX_BUFFER   = 50
-BOX               = int(len(ANCHORS)/2)
-CLASS             = len(LABELS)
-## true_boxes is the tensor that takes "b_batch"
-model, true_boxes = define_YOLOv2(IMAGE_H,IMAGE_W,GRID_H,GRID_W,TRUE_BOX_BUFFER,BOX,CLASS, 
-                                  trainable=False)
-model.summary()
+# IMAGE_H, IMAGE_W  = 416, 416
+# GRID_H,  GRID_W   = 13 , 13
+# TRUE_BOX_BUFFER   = 50
+# BOX               = int(len(ANCHORS)/2)
+# CLASS             = len(LABELS)
+# ## true_boxes is the tensor that takes "b_batch"
+# model, true_boxes = define_YOLOv2(IMAGE_H,IMAGE_W,GRID_H,GRID_W,TRUE_BOX_BUFFER,BOX,CLASS, 
+#                                   trainable=False)
+# model.summary()
 
-"""## Load pre-trained YOLOv2 weights 
-Following the instruction at [YOLO: Real-Time Object Detection](https://pjreddie.com/darknet/yolov2/), we download the pre-trained weights using wget: 
+# """## Load pre-trained YOLOv2 weights 
+# Following the instruction at [YOLO: Real-Time Object Detection](https://pjreddie.com/darknet/yolov2/), we download the pre-trained weights using wget: 
 
-<code>
-wget https://pjreddie.com/media/files/yolov2.weights
-</code>
+# <code>
+# wget https://pjreddie.com/media/files/yolov2.weights
+# </code>
 
-The weights are saved at:
-"""
+# The weights are saved at:
+# """
 
-path_to_weight = "./yolov2.weights"
+# path_to_weight = "./yolov2.weights"
 
-"""The following codes are extracted from [keras-yolo2/Yolo Step-by-Step.ipynb](https://github.com/experiencor/keras-yolo2/blob/master/Yolo%20Step-by-Step.ipynb)"""
+# """The following codes are extracted from [keras-yolo2/Yolo Step-by-Step.ipynb](https://github.com/experiencor/keras-yolo2/blob/master/Yolo%20Step-by-Step.ipynb)"""
 
-class WeightReader:
-    # code from https://github.com/experiencor/keras-yolo2/blob/master/Yolo%20Step-by-Step.ipynb
-    def __init__(self, weight_file):
-        self.offset = 4
-        self.all_weights = np.fromfile(weight_file, dtype='float32')
+# class WeightReader:
+#     # code from https://github.com/experiencor/keras-yolo2/blob/master/Yolo%20Step-by-Step.ipynb
+#     def __init__(self, weight_file):
+#         self.offset = 4
+#         self.all_weights = np.fromfile(weight_file, dtype='float32')
         
-    def read_bytes(self, size):
-        self.offset = self.offset + size
-        return self.all_weights[self.offset-size:self.offset]
+#     def read_bytes(self, size):
+#         self.offset = self.offset + size
+#         return self.all_weights[self.offset-size:self.offset]
     
-    def reset(self):
-        self.offset = 4
+#     def reset(self):
+#         self.offset = 4
                 
-weight_reader = WeightReader(path_to_weight)
-print("all_weights.shape = {}".format(weight_reader.all_weights.shape))
+# weight_reader = WeightReader(path_to_weight)
+# print("all_weights.shape = {}".format(weight_reader.all_weights.shape))
 
-"""Assign pre-trained weights to the following layers: 
-<code> conv_i</code>, <code>norm_i</code>, <code> i = 1, 2,..., 22</code>. 
-These layers do not depend on the number of object classes or the number of anchor boxes.
-"""
+# """Assign pre-trained weights to the following layers: 
+# <code> conv_i</code>, <code>norm_i</code>, <code> i = 1, 2,..., 22</code>. 
+# These layers do not depend on the number of object classes or the number of anchor boxes.
+# """
 
-def set_pretrained_weight(model,nb_conv, path_to_weight):
-    weight_reader = WeightReader(path_to_weight)
-    weight_reader.reset()
-    for i in range(1, nb_conv+1):
-        conv_layer = model.get_layer('conv_' + str(i)) ## convolusional layer
+# def set_pretrained_weight(model,nb_conv, path_to_weight):
+#     weight_reader = WeightReader(path_to_weight)
+#     weight_reader.reset()
+#     for i in range(1, nb_conv+1):
+#         conv_layer = model.get_layer('conv_' + str(i)) ## convolusional layer
 
-        if i < nb_conv:
-            norm_layer = model.get_layer('norm_' + str(i)) ## batch normalization layer
+#         if i < nb_conv:
+#             norm_layer = model.get_layer('norm_' + str(i)) ## batch normalization layer
 
-            size = np.prod(norm_layer.get_weights()[0].shape)
+#             size = np.prod(norm_layer.get_weights()[0].shape)
 
-            beta  = weight_reader.read_bytes(size)
-            gamma = weight_reader.read_bytes(size)
-            mean  = weight_reader.read_bytes(size)
-            var   = weight_reader.read_bytes(size)
+#             beta  = weight_reader.read_bytes(size)
+#             gamma = weight_reader.read_bytes(size)
+#             mean  = weight_reader.read_bytes(size)
+#             var   = weight_reader.read_bytes(size)
 
-            weights = norm_layer.set_weights([gamma, beta, mean, var])       
+#             weights = norm_layer.set_weights([gamma, beta, mean, var])       
 
-        if len(conv_layer.get_weights()) > 1: ## with bias
-            bias   = weight_reader.read_bytes(np.prod(conv_layer.get_weights()[1].shape))
-            kernel = weight_reader.read_bytes(np.prod(conv_layer.get_weights()[0].shape))
-            kernel = kernel.reshape(list(reversed(conv_layer.get_weights()[0].shape)))
-            kernel = kernel.transpose([2,3,1,0])
-            conv_layer.set_weights([kernel, bias])
-        else: ## without bias
-            kernel = weight_reader.read_bytes(np.prod(conv_layer.get_weights()[0].shape))
-            kernel = kernel.reshape(list(reversed(conv_layer.get_weights()[0].shape)))
-            kernel = kernel.transpose([2,3,1,0])
-            conv_layer.set_weights([kernel])
-    return(model)
+#         if len(conv_layer.get_weights()) > 1: ## with bias
+#             bias   = weight_reader.read_bytes(np.prod(conv_layer.get_weights()[1].shape))
+#             kernel = weight_reader.read_bytes(np.prod(conv_layer.get_weights()[0].shape))
+#             kernel = kernel.reshape(list(reversed(conv_layer.get_weights()[0].shape)))
+#             kernel = kernel.transpose([2,3,1,0])
+#             conv_layer.set_weights([kernel, bias])
+#         else: ## without bias
+#             kernel = weight_reader.read_bytes(np.prod(conv_layer.get_weights()[0].shape))
+#             kernel = kernel.reshape(list(reversed(conv_layer.get_weights()[0].shape)))
+#             kernel = kernel.transpose([2,3,1,0])
+#             conv_layer.set_weights([kernel])
+#     return(model)
 
 
-nb_conv = 22
-model = set_pretrained_weight(model,nb_conv, path_to_weight)
+# nb_conv = 22
+# model = set_pretrained_weight(model,nb_conv, path_to_weight)
 
-"""## Initialize the 23rd layer"""
+# """## Initialize the 23rd layer"""
 
-def initialize_weight(layer,sd):
-    weights = layer.get_weights()
-    new_kernel = np.random.normal(size=weights[0].shape, scale=sd)
-    new_bias   = np.random.normal(size=weights[1].shape, scale=sd)
-    layer.set_weights([new_kernel, new_bias])
+# def initialize_weight(layer,sd):
+#     weights = layer.get_weights()
+#     new_kernel = np.random.normal(size=weights[0].shape, scale=sd)
+#     new_bias   = np.random.normal(size=weights[1].shape, scale=sd)
+#     layer.set_weights([new_kernel, new_bias])
     
-layer   = model.layers[-4] # the last convolutional layer
-initialize_weight(layer,sd=GRID_H*GRID_W)
+# layer   = model.layers[-4] # the last convolutional layer
+# initialize_weight(layer,sd=GRID_H*GRID_W)
 
-"""So that's how we define the YOLOv2 model!
-The next blog will discuss the loss function of this model which will be used to train the parameters.
+# """So that's how we define the YOLOv2 model!
+# The next blog will discuss the loss function of this model which will be used to train the parameters.
 
-[FairyOnIce/ObjectDetectionYolo](https://github.com/FairyOnIce/ObjectDetectionYolo)
- contains this ipython notebook and all the functions that I defined in this notebook. 
-"""
+# [FairyOnIce/ObjectDetectionYolo](https://github.com/FairyOnIce/ObjectDetectionYolo)
+#  contains this ipython notebook and all the functions that I defined in this notebook. 
+# """
+# print("END OF SCRIPT!")
