2c5faee2580a1d7919c66420f4a66d7877094e55
Mart Bluiminck VS <m.bluiminck@dgs-ps.nl>
2023-03-09 14:00:36 +0100

    121211

--- ObjectDetectionYolo-master/part_5_object_detection_with_yolo_using_voc_2012_data_training.py
+++ ObjectDetectionYolo-master/part_5_object_detection_with_yolo_using_voc_2012_data_training.py
@@ -212,19 +212,19 @@ early_stop = EarlyStopping(monitor='loss',
                            verbose=1)
 print("Ik ben hiero2")
 
-# checkpoint = ModelCheckpoint('weights_yolo_on_voc2012.h5', 
-#                              monitor='loss', 
-#                              verbose=1, 
-#                              save_best_only=True, 
-#                              mode='min',
-#                              save_freq='epoch', 
-#                              period=10)
+checkpoint = ModelCheckpoint('weights_yolo_on_voc2012.h5', 
+                             monitor='loss', 
+                             verbose=1, 
+                             save_best_only=True, 
+                             mode='min',
+                             save_freq='epoch', 
+                             period=10)
 
-# print("Ik ben hiero3")
+print("Ik ben hiero3")
 
-# optimizer = Adam(lr=0.5e-4, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
-# optimizer = SGD(lr=1e-4, decay=0.0005, momentum=0.9)
-# optimizer = RMSprop(lr=1e-4, rho=0.9, epsilon=1e-08, decay=0.0)
+optimizer = Adam(lr=0.5e-4, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
+optimizer = SGD(lr=1e-4, decay=0.0005, momentum=0.9)
+optimizer = RMSprop(lr=1e-4, rho=0.9, epsilon=1e-08, decay=0.0)
 
 # model.compile(loss=custom_loss, optimizer=optimizer)
 
