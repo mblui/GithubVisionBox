515551ae5b0399b3033cc95ff068ab891a127c06
Mart Bluiminck VS <m.bluiminck@dgs-ps.nl>
2023-03-09 14:00:12 +0100

    12121

--- ObjectDetectionYolo-master/part_5_object_detection_with_yolo_using_voc_2012_data_training.py
+++ ObjectDetectionYolo-master/part_5_object_detection_with_yolo_using_voc_2012_data_training.py
@@ -202,15 +202,15 @@ except:
     pass
 
 
-# BATCH_SIZE   = 32
-# generator_config['BATCH_SIZE'] = BATCH_SIZE
-# print("Ik ben hiero")
-# early_stop = EarlyStopping(monitor='loss', 
-#                            min_delta=0.001, 
-#                            patience=3, 
-#                            mode='min', 
-#                            verbose=1)
-# print("Ik ben hiero2")
+BATCH_SIZE   = 32
+generator_config['BATCH_SIZE'] = BATCH_SIZE
+print("Ik ben hiero")
+early_stop = EarlyStopping(monitor='loss', 
+                           min_delta=0.001, 
+                           patience=3, 
+                           mode='min', 
+                           verbose=1)
+print("Ik ben hiero2")
 
 # checkpoint = ModelCheckpoint('weights_yolo_on_voc2012.h5', 
 #                              monitor='loss', 
