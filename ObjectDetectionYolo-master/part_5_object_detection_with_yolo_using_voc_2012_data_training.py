00a7ad3b3c00643876109937af9a23e919fb99b2
Mart Bluiminck | DGS Processing Solutions <m.bluiminck@dgs-ps.nl>
2023-03-10 13:29:50 +0100

    decrease batchsize

--- ObjectDetectionYolo-master/part_5_object_detection_with_yolo_using_voc_2012_data_training.py
+++ ObjectDetectionYolo-master/part_5_object_detection_with_yolo_using_voc_2012_data_training.py
@@ -211,7 +211,7 @@ except:
     pass
 
 
-BATCH_SIZE   = 3
+BATCH_SIZE   = 1
 generator_config['BATCH_SIZE'] = BATCH_SIZE
 early_stop = EarlyStopping(monitor='loss', 
                            min_delta=0.01, 
