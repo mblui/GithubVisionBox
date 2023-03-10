6af0a83078db15245ef209eb4cd87d65849e394d
Mart Bluiminck | DGS Processing Solutions <m.bluiminck@dgs-ps.nl>
2023-03-10 13:59:04 +0100

    121212

--- ObjectDetectionYolo-master/part_5_object_detection_with_yolo_using_voc_2012_data_training.py
+++ ObjectDetectionYolo-master/part_5_object_detection_with_yolo_using_voc_2012_data_training.py
@@ -109,7 +109,7 @@ This script can be downloaded at [my Github repository, FairyOnIce/ObjectDetecti
 from backend import SimpleBatchGenerator
 
 BATCH_SIZE        = 5           # Initial: 500
-IMAGE_H, IMAGE_W  = 416, 416    # Initial: 416, 416
+IMAGE_H, IMAGE_W  = 218, 218    # Initial: 416, 416
 GRID_H,  GRID_W   = 13 , 13     # Initial: 13,13
 TRUE_BOX_BUFFER   = 50          # Initial: 50
 BOX               = int(len(ANCHORS)/2)
