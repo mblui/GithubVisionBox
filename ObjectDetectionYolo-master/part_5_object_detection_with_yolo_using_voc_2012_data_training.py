449e04d2771a49f72c52781733379dd78594f5c4
Mart Bluiminck VS <m.bluiminck@dgs-ps.nl>
2023-03-13 10:02:28 +0100

    1212121

--- ObjectDetectionYolo-master/part_5_object_detection_with_yolo_using_voc_2012_data_training.py
+++ ObjectDetectionYolo-master/part_5_object_detection_with_yolo_using_voc_2012_data_training.py
@@ -240,7 +240,7 @@ model.compile(loss=custom_loss, optimizer=optimizer, experimental_run_tf_functio
 model.run_eagerly = False
 model.fit(train_batch_generator, 
             steps_per_epoch  = len(train_batch_generator), 
-            epochs           = 3, 
+            epochs           = 5, 
             verbose          = 1,
             #validation_data  = valid_batch,
             #validation_steps = len(valid_batch),
